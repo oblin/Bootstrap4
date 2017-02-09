8e538ce5860d73c1bcc621e2ce1023a26aedbfa2
yiming lin <yiming.lin@outlook.com>
2017-02-09 22:01:53 +0800

    Using Bootstrap JavaScript Components Cascading Dropdowns

---
+++ src/Bootstrap4/Models/EmployeeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootstrap4.Models
+{
+
+    public class EmployeeModel
+    {
+        public EmployeeModel()
+        {
+            ReportingEmployees = new List<EmployeeModel>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string About { get; set; }
+
+        public int? ReportsTo { get; set; }
+
+
+        public List<EmployeeModel> ReportingEmployees { get; set; }
+
+    }
+}
